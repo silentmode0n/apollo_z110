78ee4b27ef87605c648578ec6f7c1ce695efedff
silentmode0n <as.makar@ya.ru>
2025-10-15 23:34:15 +0400

    write set_data() for ProductGroup

--- app/gui.py
+++ app/gui.py
@@ -122,8 +122,8 @@ class ProductGroup(QGroupBox):
         )
         self.color_line = QLineEdit(form)
         self.color_line.setValidator(QIntValidator(bottom=0))
-        self.color_type_line = QComboBox(form)
-        self.color_type_line.addItems(COLOR_TYPES)
+        self.colortype_line = QComboBox(form)
+        self.colortype_line.addItems(COLOR_TYPES)
         self.num_line = QLineEdit(form)
         self.num_line.setValidator(QIntValidator(bottom=0))
         self.num_line.textChanged.connect(
@@ -135,7 +135,7 @@ class ProductGroup(QGroupBox):
         form_layout.addRow(TABLE_HEADERS[0], self.width_line)
         form_layout.addRow(TABLE_HEADERS[1], self.height_line)
         form_layout.addRow(TABLE_HEADERS[2], self.color_line)
-        form_layout.addRow(TABLE_HEADERS[3], self.color_type_line)
+        form_layout.addRow(TABLE_HEADERS[3], self.colortype_line)
         form_layout.addRow(TABLE_HEADERS[4], self.num_line)
         form_layout.addRow(add_button)
 
@@ -153,12 +153,12 @@ class ProductGroup(QGroupBox):
         width = self.width_line.text().strip()
         height = self.height_line.text().strip()
         color = self.color_line.text().strip()
-        color_type = self.color_type_line.currentText().strip()
+        colortype = self.colortype_line.currentText().strip()
         num = self.num_line.text().strip()
-        return width, height, color, color_type, num
+        return width, height, color, colortype, num
 
     def form_is_valid(self):
-        width, height, color, color_type, num = self.get_form_values()
+        width, height, color, colortype, num = self.get_form_values()
         if not width or not height or not color or not num:
             return False
         return True
@@ -187,18 +187,20 @@ class ProductGroup(QGroupBox):
             width = self.table.item(row, 0)
             height = self.table.item(row, 1)
             color = self.table.item(row, 2)
-            color_type = self.table.item(row, 3)
+            colortype = self.table.item(row, 3)
             num = self.table.item(row, 4)
             row_values["width"] = width.text() if width else ''
             row_values['height'] = height.text() if height else ''
             row_values['color'] = color.text() if color else ''
-            row_values['color_type'] = color_type.text() if color_type else ''
+            row_values['color_type'] = colortype.text() if colortype else ''
             row_values['num'] = num.text() if num else ''
             values.append(row_values)
         return {'table': values}
 
-    def set_data(self, data: dict): 
-        pass # TODO: write set_data for ProductGroup
+    def set_data(self, data: dict): # dict: {products: List["width_height_color_colortype_num"]}
+        if 'products' in data:
+            for product in data['products']:
+                self.insert_row(product.split('_'))
 
 
 class CommentGroup(QGroupBox):
