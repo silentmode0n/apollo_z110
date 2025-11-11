7f42d5484022a1eac0017f4e4fe2eee9d30c97bb
silentmode0n <as.makar@ya.ru>
2025-11-11 21:21:10 +0400

    remove typing for python3.8

--- app/model.py
+++ app/model.py
@@ -13,7 +13,7 @@ from .config import (
 
 class Element:
 
-    def __init__(self, name: str, size: int, color: str, colortype: str, unit: str):
+    def __init__(self, name, size, color, colortype, unit):
         self.name = name
         self.size = size
         self.color = color
@@ -23,7 +23,7 @@ class Element:
     def __repr__(self):
         return f"{self.__class__.__name__} <{self.name} {self.size} RAL{self.color} {self.colortype}>"
 
-    def __hash__(self) -> int:
+    def __hash__(self):
         return hash(self.__repr__())
 
     def __eq__(self, obj):
@@ -45,17 +45,17 @@ class Counter:
     def __init__(self):
         self._map = {}
 
-    def add(self, new_element: Element, num: int):
+    def add(self, new_element, num):
         if num > 0:
             if new_element in self._map.keys():
                 self._map[new_element] += num
             else:
                 self._map[new_element] = num
 
-    def get_dict(self) -> dict[Element, int]:
+    def get_dict(self):
         return self._map.copy()
 
-    def get(self) -> list[dict]:
+    def get(self):
         list_of_elements = []
         for element, count in self._map.items():
             list_of_elements.append({
@@ -71,27 +71,27 @@ class Counter:
 
 
 class Slat(Element):
-	def __init__(self, size: int, color: str, colortype: str):
+	def __init__(self, size, color, colortype):
 		super().__init__(name=SLAT_TITLE, size=size, color=color, colortype=colortype, unit=SLAT_UNITS)
 
 
 class Cap(Element):
-	def __init__(self, size: int, color: str, colortype: str):
+	def __init__(self, size, color, colortype):
 		super().__init__(name=CAP_TITLE, size=size, color=color, colortype=colortype, unit=CAP_UNITS)
 
 
 class Rail(Element):
-	def __init__(self, size: int, color: str, colortype: str):
+	def __init__(self, size, color, colortype):
 		super().__init__(name=RAIL_TITLE, size=size, color=color, colortype=colortype, unit=RAIL_UNITS)
 
 
 class Lamel(Element):
-	def __init__(self, size: int, color: str, colortype: str):
+	def __init__(self, size, color, colortype):
 		super().__init__(name=LAMEL_TITLE, size=size, color=color, colortype=colortype, unit=LAMEL_UNITS)
 
 
 class Fence:
-    def __init__(self, width: int, height: int, color: str, colortype: str):
+    def __init__(self, width, height, color, colortype):
         self.name = FENCE_TITLE
         self.width = width
         self.height = height
@@ -99,30 +99,30 @@ class Fence:
         self.colortype = colortype
         self.slat_num = 2 if self.width >= 2500 else 1 if self.width >= 2000 else 0
 
-    def __repr__(self) -> str:
+    def __repr__(self):
           return f'{self.__class__.__name__} <{self.name} {self.width}x{self.height} RAL{self.color} {self.colortype}>'
 
-    def get_lamels(self) -> tuple[Element, int]:
+    def get_lamels(self):
         num = (self.height // 110)
         return (Lamel(self.width - 15, self.color, self.colortype), num)
 
-    def get_rails(self) -> tuple[Element, int]:
+    def get_rails(self):
         return (Rail(self.height, self.color, self.colortype), 1)
 
-    def get_caps(self) -> tuple[Element, int]:
+    def get_caps(self):
         return (Cap(self.width, self.color, self.colortype), 1)
 
-    def get_slats(self) -> tuple[Element, int]:
+    def get_slats(self):
         return (Slat(self.height - 40, self.color, self.colortype), self.slat_num)
 
 
 class Model:
     """Класс описывающий главную модель"""
 
-    def __init__(self, raw_table: list[dict]):
+    def __init__(self, raw_table):
         self.fences = self._create_fences_from_tabel(raw_table)
 
-    def _create_fences_from_tabel(self, raw_table: list) -> list[tuple[Fence, int]]:
+    def _create_fences_from_tabel(self, raw_table):
         fences = []
         for row in raw_table:
             fences.append((
@@ -131,7 +131,7 @@ class Model:
                 ))
         return fences
 
-    def export_fences(self) -> list[dict]:
+    def export_fences(self):
         data = []
         for fence, count in self.fences:
             data.append({
@@ -145,28 +145,28 @@ class Model:
                 })
         return data
 
-    def export_lamels(self) -> list[dict]:
+    def export_lamels(self):
         counter = Counter()
         for fence, count in self.fences:
             element, num = fence.get_lamels()
             counter.add(element, num * count)
         return counter.get()
 
-    def export_rails(self) -> list[dict]:
+    def export_rails(self):
         counter = Counter()
         for fence, count in self.fences:
             element, num = fence.get_rails()
             counter.add(element, num * count)
         return counter.get()
 
-    def export_caps(self) -> list[dict]:
+    def export_caps(self):
         counter = Counter()
         for fence, count in self.fences:
             element, num = fence.get_caps()
             counter.add(element, num * count)
         return counter.get()
 
-    def export_slats(self) -> list[dict]:
+    def export_slats(self):
         counter = Counter()
         for fence, count in self.fences:
             element, num = fence.get_slats()
